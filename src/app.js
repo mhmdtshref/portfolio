5f659d79c107e896b4b0fc5e0ae39d186184c02a
mhmdtshref <mhmdtshref@gmail.com>
2019-06-02 19:41:41 +0300

    Create server side express app #1

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const router = require('./router');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+
+
+module.exports = app;
