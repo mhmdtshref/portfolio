53ca679fb7232e26f8604527cffed7312c48b4e6
mhmdtshref <mhmdtshref@gmail.com>
2019-06-12 19:00:40 +0300

    Use index.js in the router to get sectionRouter route #33

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const sectionRouter = require('./router/section.router');
+const { sectionRouter } = require('./router/');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
