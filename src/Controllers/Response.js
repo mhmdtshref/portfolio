08df6c6676f53f06c846a85f8ca8f8d779c48509
mhmdtshref <mhmdtshref@gmail.com>
2019-06-13 12:19:00 +0300

    Create requestError response #36

--- src/Controllers/Response.js
+++ src/Controllers/Response.js
@@ -2,11 +2,15 @@ const dbError = (res, error) => {
     res.status(500).json({ success: false, error: error.message });
 };
 
+const requestError = (res, error) => {
+    res.json({ success: false, error: `Request Error: ${error.message}` });
+};
+
 const success = (res, data) => {
     res.json({ success: true, data });
 };
 
 module.exports = {
-    error: { database: dbError },
+    error: { database: dbError, badRequest: requestError },
     success,
 };
