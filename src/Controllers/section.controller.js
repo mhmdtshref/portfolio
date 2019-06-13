0a30b37389d0723b890cd537768c7f5a756861fb
mhmdtshref <mhmdtshref@gmail.com>
2019-06-13 14:23:22 +0300

    Create get function to get wection details by id #36

--- src/Controllers/section.controller.js
+++ src/Controllers/section.controller.js
@@ -1,18 +1,28 @@
-const { Section } = require('../Models/');
+const { Section, LogoCard, TextCard, PictureCard } = require('../Models/');
 const Response = require('./Response.js');
 
-const index = (req, res) => {
-/*    const { id } = req.params;
-    Section.findById(id)
-        .exec((err, section) => {
-            if(err){
-                Response.error.database(res, err);
-            } else {
-                const { cards } = section;
+
+const get = (req, res) => {
+    const { id } = req.params;
+    let c = 0;
+    let cards = [];
+
+    const checkFinish = counter => counter >= 3;
+
+    const submitCards = (error, cardsToAdd) => {
+        if(!error){
+            cards = cards.concat(cardsToAdd);
+            if(checkFinish(++c)){
                 Response.success(res, { cards });
-            }
-        });*/
-    Response.success(res, { cards: ['card-example-1', 'card-example-2'] });
+            };
+        } else {
+            Response.error.database(res, error);
+        }
+    };
+
+    LogoCard.find({ section: id }).exec(submitCards);
+    TextCard.find({ section: id }).exec(submitCards);
+    PictureCard.find({ section: id }).exec(submitCards);
 };
 
 const store = (req, res) => {
@@ -27,4 +37,4 @@ const store = (req, res) => {
         });
 };
 
-module.exports = { index, store };
+module.exports = { get, store };
