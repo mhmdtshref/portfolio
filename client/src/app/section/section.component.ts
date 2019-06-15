f81a82cd69d3aaad1555a4aad06bc4961979ab63
mhmdtshref <mhmdtshref@gmail.com>
2019-06-15 22:40:17 +0300

    fix tslint #35

--- client/src/app/section/section.component.ts
+++ client/src/app/section/section.component.ts
@@ -15,12 +15,12 @@ export class SectionComponent implements OnInit {
   }
 
   getCards = () => {
-    console.log("TITLE: ", this.title);
+    console.log('TITLE: ', this.title);
     request.get(`/api/section/${this.title}`)
       .then(({data: response}) => {
-        if(response.success){
+        if (response.success) {
           this.cards = this.generateSectionCards(response.data.section, response.data.cards);
-          console.log("TITLE: ", this.title,"CARDS:: ", this.cards);
+          console.log('TITLE: ', this.title, 'CARDS:: ', this.cards);
         } else {
           alert(`Response error: ${response.error}`);
         }
@@ -29,15 +29,15 @@ export class SectionComponent implements OnInit {
         alert(`Request Error: ${error.message}`);
     });
 
-  };
+  }
 
   generateSectionCards = ({ size, name }, cards) => {
-    if(cards.length > size){
-      return (cards.slice(0, size-1)).concat([ { type: 5, details: { link: `/${name}` } } ]);
+    if (cards.length > size) {
+      return (cards.slice(0, size - 1)).concat([ { type: 5, details: { link: `/${name}` } } ]);
     } else {
       return cards;
     }
-  };
+  }
 
   ngOnInit() {
     this.getCards();
