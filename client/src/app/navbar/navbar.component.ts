5b2df7b18fe40c915c577b1956ca926e43bfa7e9
mhmdtshref <mhmdtshref@gmail.com>
2019-06-30 13:54:48 +0300

    Add on scroll event handler to toggle classes #56

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
+
+  @HostListener('window:scroll', [ '$event' ])
+  scrollHandler() {
+    const navEl = document.querySelector('#nav');
+    const NavTitle = document.querySelector('#nav h3');
+    const NavItems = document.querySelectorAll('#nav #navList li a');
+    const fullHeight = window.innerHeight;
+    const percentage = (pageYOffset / fullHeight) * 100;
+    if (percentage > 30) {
+      navEl.className = 'secondary-bg';
+      NavTitle.className = 'secondary-text-color';
+      NavItems.forEach(x => x.className = 'secondary-text-color');
+    } else {
+      navEl.className = 'primary-bg';
+      NavTitle.className = 'primary-text-color';
+      NavItems.forEach(x => x.className = 'primary-text-color');
+    }
+  }
 
   ngOnInit() {
   }
