7777c9926bdd440db956a398c90ca961941077f1
mhmdtshref <mhmdtshref@gmail.com>
2019-07-03 13:48:49 +0300

    Export socials router #63

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 const sectionRouter = require('./section.router');
 const cardRouter = require('./card.router');
 const contactUsRouter = require('./contactUs.router');
+const socialRouter = require('./social.router');
 
-module.exports = { sectionRouter, cardRouter, contactUsRouter };
+module.exports = { sectionRouter, cardRouter, contactUsRouter, socialRouter };
