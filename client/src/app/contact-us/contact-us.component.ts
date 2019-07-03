531185b67b02f97517f0c1a8fbc3eb502a4ea2c9
mhmdtshref <mhmdtshref@gmail.com>
2019-07-03 11:32:08 +0300

    Add contact-us class component #60

--- client/src/app/contact-us/contact-us.component.ts
+++ client/src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import set = Reflect.set;
+
 
 @Component({
   selector: 'app-contact-us',
@@ -7,7 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  data = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  };
+
+  constructor(private http: HttpClient) {
+  }
+
+  onSubmitHandler(e) {
+    e.preventDefault();
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+
+    const params = new HttpParams()
+      .set('name', this.data.name)
+      .set('email', this.data.email)
+      .set('subject', this.data.subject)
+      .set('message', this.data.message);
+
+    this.http.post('/api/contactUs', params.toString(), { headers })
+      .subscribe((res: any) => {
+        if (res.success) {
+          alert('Your message has been submitted successfully!');
+        } else {
+          alert(`Submit error: ${res.error}`);
+        }
+      });
+  }
+
+  onChangeHandler = (e) => {
+    this.data[e.target.name] = e.target.value;
+    console.log(this.data);
+  }
 
   ngOnInit() {
   }
