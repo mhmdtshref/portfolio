02809376cdff1da601489ec07370a691034dfa3f
mhmdtshref <mhmdtshref@gmail.com>
2019-07-03 13:47:32 +0300

    Export social controller #63

--- src/Controllers/index.js
+++ src/Controllers/index.js
@@ -1,5 +1,6 @@
 const Section = require('./section.controller');
 const Card = require('./card.controller');
 const ContactUs = require('./contactUs.controller');
+const Social = require('./social.controller');
 
-module.exports = { Section, Card, ContactUs };
+module.exports = { Section, Card, ContactUs, Social };
