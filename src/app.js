516eb194e666f3032e29be34620603bae27f33e5
mhmdtshref <mhmdtshref@gmail.com>
2019-07-03 13:48:27 +0300

    Use social router in app #63

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const { sectionRouter, cardRouter, contactUsRouter } = require('./router/');
+const { sectionRouter, cardRouter, contactUsRouter, socialRouter } = require('./router/');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'dist', 'client')));
 app.use('/api/section', sectionRouter);
 app.use('/api/card', cardRouter);
 app.use('/api/contactUs', contactUsRouter);
+app.use('/api/social', socialRouter);
 
 app.get('/*', (req, res) => res.sendfile(path.join(__dirname, '..')));
 
