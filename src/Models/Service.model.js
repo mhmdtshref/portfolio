8a83b1d08b9a852f49a04965fced5dd497a990ed
mhmdtshref <mhmdtshref@gmail.com>
2019-07-06 21:37:48 +0300

    Return the models #88

---
+++ src/Models/Service.model.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose');
+
+const ServiceSchema = Schema({
+    name: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    brief: { type: String, required: true },
+    description: { type: String, required: true },
+});
+
+module.exports = model('Service', ServiceSchema);
