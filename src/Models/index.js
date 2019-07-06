77773bd0d91d2282b065a1c68c0a90bd39fb7c05
mhmdtshref <mhmdtshref@gmail.com>
2019-07-06 18:48:13 +0300

    Export models from models index.js #88

--- src/Models/index.js
+++ src/Models/index.js
@@ -6,7 +6,8 @@ const Social = require('./Social.model');
 const Project = require('./Project.model');
 const Service = require('./Service.model');
 const Technology = require('./Technology.model');
+const Language = require('./Language.model');
 
 module.exports = {
-  Section, PictureCard, TextCard, LogoCard, Social, Project, Service, Technology,
+  Section, PictureCard, TextCard, LogoCard, Social, Project, Service, Technology, Language,
 };
