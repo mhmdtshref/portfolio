db376126760cca162e367940940dea41d3074c84
mhmdtshref <mhmdtshref@gmail.com>
2019-07-06 18:27:59 +0300

    Export new routers #87

--- src/router/index.js
+++ src/router/index.js
@@ -2,5 +2,8 @@ const sectionRouter = require('./section.router');
 const cardRouter = require('./card.router');
 const contactUsRouter = require('./contactUs.router');
 const socialRouter = require('./social.router');
+const projectRouter = require('./project.router');
+const serviceRouter = require('./service.router');
+const technologyRouter = require('./technology.router');
 
-module.exports = { sectionRouter, cardRouter, contactUsRouter, socialRouter };
+module.exports = { sectionRouter, cardRouter, contactUsRouter, socialRouter, projectRouter, serviceRouter, technologyRouter };
