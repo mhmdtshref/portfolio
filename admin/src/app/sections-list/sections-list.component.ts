e87bad986854181dd08431656d69a6c590705401
mhmdtshref <mhmdtshref@gmail.com>
2019-07-11 06:50:34 +0300

    Create initial components #100

---
+++ admin/src/app/sections-list/sections-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sections-list',
+  templateUrl: './sections-list.component.html',
+  styleUrls: ['./sections-list.component.scss']
+})
+export class SectionsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
