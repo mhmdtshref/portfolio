9efaaa36fc13854159c4f13274a7bac1ef6a60ef
mhmdtshref <mhmdtshref@gmail.com>
2019-07-14 13:57:46 +0300

    Add fake projects list #115

--- admin/src/app/pages/projects/projects.component.ts
+++ admin/src/app/pages/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+
+    projects = [
+        {
+            imageUrl: 'https://res.cloudinary.com/howl1960/image/upload/v1561877296/npm-package_vlybe1.jpg',
+            name: 'npm package', id: '5d20b4845117fb6e48e59576'
+        },
+        {
+            imageUrl: 'https://res.cloudinary.com/howl1960/image/upload/v1561877296/npm-package_vlybe1.jpg',
+            name: 'npm package',
+            id: '5d20b4845117fb6e48e59576'
+        },
+        {
+            imageUrl: 'https://res.cloudinary.com/howl1960/image/upload/v1561877296/npm-package_vlybe1.jpg',
+            name: 'npm package', id: '5d20b4845117fb6e48e59576'
+        },
+        {
+            imageUrl: 'https://res.cloudinary.com/howl1960/image/upload/v1561877296/npm-package_vlybe1.jpg',
+            name: 'npm package', id: '5d20b4845117fb6e48e59576'
+        },
+        {
+            imageUrl: 'https://res.cloudinary.com/howl1960/image/upload/v1561877296/npm-package_vlybe1.jpg',
+            name: 'npm package', id: '5d20b4845117fb6e48e59576'
+        },
+    ];
 
   constructor() { }
 
