a674fb5a2a300a1a442857aad2b704c8a8749c86
mhmdtshref <mhmdtshref@gmail.com>
2019-07-14 14:51:58 +0300

    Create service to get lists for different pages #115

---
+++ admin/src/app/pages/lists.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListsService {
+
+  constructor(private http: HttpClient) { }
+
+  getProjectsList = () => new Promise( (resolve, reject) => {
+      this.http.get('/api/project').subscribe((res: any) => {
+          if(res.success) {
+              resolve(res.data);
+          } else {
+              reject(new Error(res.error));
+          }
+      });
+  })
+
+
+}
