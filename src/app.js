05fa7f7154af29d4c37b27497defac6ab45a4766
mhmdtshref <mhmdtshref@gmail.com>
2019-07-17 09:29:48 +0300

    Modify routes to be able to use admin app #128

--- src/app.js
+++ src/app.js
@@ -16,7 +16,9 @@ const {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist', 'client')));
+
+app.use('/admin', express.static(path.join(__dirname, '..', 'admin', 'dist', 'admin')));
+app.use('', express.static(path.join(__dirname, '..', 'client', 'dist', 'client')));
 
 app.use('/api/section', sectionRouter);
 app.use('/api/card', cardRouter);
