958d5d14482781011a89baec68bfd8f5032be64a
mhmdtshref <mhmdtshref@gmail.com>
2019-07-19 00:59:15 +0300

    Add create project request service #141

--- admin/src/app/usable/requests.service.ts
+++ admin/src/app/usable/requests.service.ts
@@ -8,7 +8,7 @@ export class RequestsService {
 
   constructor(private http: HttpClient) { }
 
-  deleteProject = (id) => new Promise((resolve, reject) => {
+    deleteProject = (id) => new Promise((resolve, reject) => {
       this.http.delete(`/api/project/${id}`)
           .subscribe((res: any) => {
               if (res.success) {
@@ -40,6 +40,26 @@ export class RequestsService {
 
         const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
         this.http.patch(`/api/project/${id}`, params.toString(), { headers })
+            .subscribe((res: any) => {
+                if (res.success) {
+                    resolve();
+                } else {
+                    reject(new Error(res.error));
+                }
+            });
+    })
+
+    createProject = (project) => new Promise((resolve, reject) => {
+        const params = new HttpParams()
+            .set('name', project.name)
+            .set('link', project.link)
+            .set('gitLink', project.gitLink)
+            .set('imageUrl', project.imageUrl)
+            .set('teamWork', project.teamWork)
+            .set('description', project.description);
+
+        const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        this.http.post(`/api/project`, params.toString(), { headers })
             .subscribe((res: any) => {
                 if (res.success) {
                     resolve();
