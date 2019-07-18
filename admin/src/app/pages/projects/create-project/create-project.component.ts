29bc2fe79ad2ee35d48de67e7d886f05b3490cd2
mhmdtshref <mhmdtshref@gmail.com>
2019-07-19 01:07:52 +0300

    Create required functions and variables for create-projects component #141

--- admin/src/app/pages/projects/create-project/create-project.component.ts
+++ admin/src/app/pages/projects/create-project/create-project.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestsService } from "../../../usable/requests.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-create-project',
@@ -7,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private requestsServices: RequestsService, private router: Router) { }
+
+  project = { name: null, link: null, gitLink: null, imageUrl: null, teamWork: false, description: null };
+
+  saveHandler = () => {
+      if (this.project.name
+          && this.project.link
+          && this.project.gitLink
+          && this.project.imageUrl
+          && this.project.description !== null
+      ) {
+          this.requestsServices.createProject(this.project).then(() => {
+              this.router.navigate(['projects']);
+          }).catch((error) => {
+              alert(`Error: ${error.message}`);
+          })
+      } else {
+          alert('Fill all fields please!');
+      }
+  }
 
   ngOnInit() {
   }
