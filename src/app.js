935f38374ceda14e650c498561019db8c80ca00a
mhmdtshref <mhmdtshref@gmail.com>
2019-07-18 16:49:50 +0300

    Use express fallback package to chack if the file is found and redirect if needed #139

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const fallback = require('express-history-api-fallback');
 
 const app = express();
 
@@ -17,9 +18,6 @@ const {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/admin', express.static(path.join(__dirname, '..', 'admin', 'dist', 'admin')));
-app.use('', express.static(path.join(__dirname, '..', 'client', 'dist', 'client')));
-
 app.use('/api/section', sectionRouter);
 app.use('/api/card', cardRouter);
 app.use('/api/contactUs', contactUsRouter);
@@ -29,6 +27,16 @@ app.use('/api/project', projectRouter);
 app.use('/api/service', serviceRouter);
 app.use('/api/technology', technologyRouter);
 app.use('/api/language', languageRouter);
+
+
+const adminRoot = path.join(__dirname, '..', 'admin', 'dist', 'admin');
+const clientRoot = path.join(__dirname, '..', 'client', 'dist', 'client');
+
+app.use('/admin', express.static(adminRoot));
+app.use('/admin', fallback('index.html', { root: adminRoot }));
+
+app.use('', express.static(clientRoot));
+app.use('', fallback('index.html', { root: clientRoot }));
 
 
 app.get('/*', (req, res) => res.sendfile(path.join(__dirname, '..')));
