6ece08cf5a37df3e719aa07441247b03bea78822
mhmdtshref <mhmdtshref@gmail.com>
2019-07-18 14:13:46 +0300

    Add required parameters and functions to project-view component class #137

--- admin/src/app/pages/projects/project-view/project-view.component.ts
+++ admin/src/app/pages/projects/project-view/project-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RequestsService} from '../../../usable/requests.service';
+import {ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-view',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private requestsServices: RequestsService, private activatedRouter: ActivatedRoute, private router: Router) { }
+
+  id = '';
+  project = {};
+  projectKeys = [ 'link', 'gitLink', 'teamWork', 'imageUrl', 'description'];
+
+  deleteClicked = false;
+
+  toRegularString = (str) => {
+      return str
+          .replace(/([A-Z])/g, ' $1')
+          .replace(/^./, (replacedStr) => replacedStr.toUpperCase());
+  }
+
+
+    deleteAction = (confirmState) => {
+        if (confirmState) {
+            this.requestsServices.deleteProject(this.id)
+                .then(() => {
+                    this.router.navigate(['projects']);
+                })
+                .catch((error) => {
+                    alert(`Error: ${error.message}`);
+                });
+        } else {
+            this.deleteClicked = false;
+        }
+    }
+
+    onClickDeleteButton = () => {
+        this.deleteClicked = true;
+    }
+
+    onClickEditButton = () => {
+        this.router.navigate(['projects', this.id, 'edit']);
+    }
+
 
   ngOnInit() {
+      this.id = this.activatedRouter.snapshot.paramMap.get('id');
+      this.requestsServices.getProject(this.id).then((project) => {
+          this.project = project;
+          this.project[this.projectKeys[3]] = this.project[this.projectKeys[3]] ? 'Yes' : 'No';
+      }).catch((error) => {
+          alert(`Error: ${error.message}`);
+      });
   }
 
 }
