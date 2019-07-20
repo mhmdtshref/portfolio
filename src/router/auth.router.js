8ceea241e3320836f982aaab109e0fc00ef55407
mhmdtshref <mhmdtshref@gmail.com>
2019-07-20 08:48:22 +0300

    Add route to auth route for check auth #144

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -4,5 +4,6 @@ const { Auth } = require('../Controllers/');
 const router = express.Router();
 
 router.post('/login', Auth.login);
+router.get('/checkAuth', Auth.checkAuth);
 
 module.exports = router;
