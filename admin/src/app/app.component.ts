b21982ccaf124b976a59c4d05cadff3acae88f07
mhmdtshref <mhmdtshref@gmail.com>
2019-07-20 09:34:13 +0300

    Create authentication function when open the app #144

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./usable/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'admin';
+
+  constructor(private authServices: AuthService, private router: Router){ }
+
+  ngOnInit() {
+      this.authServices.checkAuth().catch((error) => {
+          this.router.navigate(['login']);
+      });
+  }
 }
