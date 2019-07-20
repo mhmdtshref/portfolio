5ced02f1edae4c9d2056d93e3ee7c5bda39c87a1
mhmdtshref <mhmdtshref@gmail.com>
2019-07-20 17:52:01 +0300

    Remove login route #144

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { MainComponent } from './pages/main/main.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import {ProjectEditComponent} from './pages/projects/project-edit/project-edit.component';
 import {ProjectViewComponent} from './pages/projects/project-view/project-view.component';
-import {CreateProjectComponent} from "./pages/projects/create-project/create-project.component";
-import { LoginComponent } from './pages/login/login.component';
+import {CreateProjectComponent} from './pages/projects/create-project/create-project.component';
 
 const routes: Routes = [
     { path: '', component: MainComponent },
@@ -13,7 +12,6 @@ const routes: Routes = [
     { path: 'projects/:id/edit', component: ProjectEditComponent },
     { path: 'projects/create', component: CreateProjectComponent },
     { path: 'projects/:id', component: ProjectViewComponent },
-    { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
