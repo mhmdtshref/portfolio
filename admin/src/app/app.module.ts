f76ff81526b6dbf6aa896b2da1ab29d9de1fe008
mhmdtshref <mhmdtshref@gmail.com>
2019-07-20 09:41:51 +0300

    Add cookies service to providers #144

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from "ngx-cookie-service";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,7 @@ import { CreateProjectComponent } from './pages/projects/create-project/create-p
         HttpClientModule,
         FormsModule,
     ],
-  providers: [],
+  providers: [ CookieService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
