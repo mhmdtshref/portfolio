8febbbab685c8d48d84aefee31053697eac77a46
mhmdtshref <mhmdtshref@gmail.com>
2019-07-20 18:10:56 +0300

    Update ngOnInit function and add required parameter for authentication #144

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService} from "./usable/auth.service";
-import {Router} from "@angular/router";
+import {AuthService} from './usable/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +10,24 @@ import {Router} from "@angular/router";
 export class AppComponent implements OnInit{
   title = 'admin';
 
+  auth: boolean;
+
   constructor(private authServices: AuthService, private router: Router){ }
 
+  removeAuth = () => {
+      this.auth = false;
+  }
+
+  setAuth = () => {
+      this.auth = true;
+  }
+
   ngOnInit() {
-      this.authServices.checkAuth().catch((error) => {
-          this.router.navigate(['login']);
+      this.authServices.checkAuth()
+          .then(() => {
+              this.auth = true;
+          }).catch((error) => {
+          this.auth = false;
       });
   }
 }
