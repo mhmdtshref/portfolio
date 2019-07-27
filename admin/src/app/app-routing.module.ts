dbf901eeb667dc86d11d583b3fb63a183f2c8b38
mhmdtshref <mhmdtshref@gmail.com>
2019-07-27 09:11:00 +0300

    Create route for services page #129

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ProjectsComponent } from './pages/projects/projects.component';
 import {ProjectEditComponent} from './pages/projects/project-edit/project-edit.component';
 import {ProjectViewComponent} from './pages/projects/project-view/project-view.component';
 import {CreateProjectComponent} from './pages/projects/create-project/create-project.component';
+import {ServicesComponent} from './pages/services/services.component';
 
 const routes: Routes = [
     { path: '', component: MainComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
     { path: 'projects/:id/edit', component: ProjectEditComponent },
     { path: 'projects/create', component: CreateProjectComponent },
     { path: 'projects/:id', component: ProjectViewComponent },
+    { path: 'services', component: ServicesComponent }
 ];
 
 @NgModule({
