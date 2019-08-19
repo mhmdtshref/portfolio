896b57b9ad9dc8d41aac7b802c1bfa56c17e694e
mhmdtshref <mhmdtshref@gmail.com>
2019-08-19 18:27:12 +0300

    Create click event function to navigate to service view page #150

--- admin/src/app/usable/service/service.component.ts
+++ admin/src/app/usable/service/service.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-service',
@@ -11,7 +12,11 @@ export class ServiceComponent implements OnInit {
     @Input() service: any;
     @Input() refreshList: any;
 
-    constructor() { }
+    constructor(private router: Router) { }
+
+    toServiceView = () => {
+        this.router.navigate(['services', this.service._id]);
+    }
 
     ngOnInit() {
 
