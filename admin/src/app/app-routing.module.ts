15f872585ba757c131865450535a8c247628805d
mhmdtshref <mhmdtshref@gmail.com>
2019-08-19 13:03:05 +0300

    Add frontend admin route to create-service page component #151

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {ProjectEditComponent} from './pages/projects/project-edit/project-edit.c
 import {ProjectViewComponent} from './pages/projects/project-view/project-view.component';
 import {CreateProjectComponent} from './pages/projects/create-project/create-project.component';
 import {ServicesComponent} from './pages/services/services.component';
+import {CreateServiceComponent} from "./pages/services/create-service/create-service.component";
 
 const routes: Routes = [
     { path: '', component: MainComponent },
@@ -13,7 +14,8 @@ const routes: Routes = [
     { path: 'projects/:id/edit', component: ProjectEditComponent },
     { path: 'projects/create', component: CreateProjectComponent },
     { path: 'projects/:id', component: ProjectViewComponent },
-    { path: 'services', component: ServicesComponent }
+    { path: 'services', component: ServicesComponent },
+    { path: 'services/create', component: CreateServiceComponent },
 ];
 
 @NgModule({
