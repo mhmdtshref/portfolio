f7ed4c306a9a97ff64711ef419498d7d92c1aa97
mhmdtshref <mhmdtshref@gmail.com>
2019-08-20 06:37:49 +0300

    Add required functions and variables to the edit-service page component #130

--- admin/src/app/pages/services/service-edit/service-edit.component.ts
+++ admin/src/app/pages/services/service-edit/service-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RequestsService} from '../../../usable/requests.service';
 
 @Component({
   selector: 'app-service-edit',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRouter: ActivatedRoute, private requestService: RequestsService, private router: Router) { }
+
+    id = '';
+    loading = 'Loading ...';
+    name = this.loading;
+    disabled = true;
+    service = {
+        name: this.loading,
+        imageUrl: this.loading,
+        brief: this.loading,
+        description: this.loading,
+    };
+
+    saveHandler = (event) => {
+        event.preventDefault();
+        this.requestService.editService(this.id, this.service).then(() => {
+            this.router.navigate(['services', this.id]);
+        }).catch((error) => {
+            alert(`Error: ${error.message}`);
+        });
+    }
 
   ngOnInit() {
+      this.id = this.activatedRouter.snapshot.paramMap.get('id');
+      this.requestService.getService(this.id)
+          .then((service: any) => {
+              this.service = service;
+              this.name = service.name;
+              this.disabled = null;
+          })
+          .catch((error) => {
+              alert(`Error: ${error.message}`);
+          });
   }
 
 }
