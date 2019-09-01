9c4dfac53b9eab200bb559b00bf8fa457c981610
mhmdtshref <mhmdtshref@gmail.com>
2019-09-01 23:27:31 +0300

    Update getProject function to use the request of the project and get ID from the header instead of fake data #181

--- client/src/app/pages/projects/project-details/project-details.component.ts
+++ client/src/app/pages/projects/project-details/project-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { projects } from '../../../../appData.json';
+import {RequestsService} from '../../requests.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-project-details',
@@ -8,23 +10,33 @@ import { projects } from '../../../../appData.json';
 })
 export class ProjectDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private requestsService: RequestsService, private activatedRoute: ActivatedRoute) { }
 
   data = projects;
   details = { name: '', link: '', gitLink: '', description: '', imageUrl: '' };
+  id = null;
 
   getDetails = (id) => {
-    this.details = {
-      name: 'npm package',
-      link: 'https://www.npmjs.com/package/form2',
-      gitLink: 'https://github.com/mhmdtshref/form2',
-      description: 'npm package for ReactJS technology that helps to create forms without need to use state.',
-      imageUrl: 'https://res.cloudinary.com/howl1960/image/upload/v1561877296/npm-package_vlybe1.jpg'
-    };
+    this.requestsService.getProject(id)
+      .then((projectDetails: any) => {
+        this.details = projectDetails;
+      })
+      .catch((err) => {
+        alert(`Error: ${err.message}`);
+      });
+
+    // this.details = {
+    //   name: 'npm package',
+    //   link: 'https://www.npmjs.com/package/form2',
+    //   gitLink: 'https://github.com/mhmdtshref/form2',
+    //   description: 'npm package for ReactJS technology that helps to create forms without need to use state.',
+    //   imageUrl: 'https://res.cloudinary.com/howl1960/image/upload/v1561877296/npm-package_vlybe1.jpg'
+    // };
   }
 
   ngOnInit() {
-    this.getDetails(1);
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.getDetails(this.id);
   }
 
 }
