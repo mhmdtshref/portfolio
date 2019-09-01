9e8ed2558be1b83b2aef7b7160f9bc868090327a
mhmdtshref <mhmdtshref@gmail.com>
2019-09-01 23:26:09 +0300

    Create requests service with getProject function in it #181

---
+++ client/src/app/pages/requests.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestsService {
+
+  constructor(private http: HttpClient) { }
+
+  getProject = (id) => new Promise((resolve, reject) => {
+    this.http.get(`/api/project/${id}`).subscribe((res: any) => {
+      if (res.success) {
+        resolve(res.data);
+      } else {
+        reject(res.error);
+      }
+    });
+  })
+
+}
