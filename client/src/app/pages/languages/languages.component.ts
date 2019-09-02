00c5b107914c2fe14b62763c811a9e38cd11bfe3
mhmdtshref <mhmdtshref@gmail.com>
2019-09-02 14:33:17 +0300

    import languages data from the json file updated #208

--- client/src/app/pages/languages/languages.component.ts
+++ client/src/app/pages/languages/languages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { languages } from '../../../appData.json';
 
 @Component({
   selector: 'app-languages',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class LanguagesComponent implements OnInit {
 
   constructor() { }
+
+  languagesData = languages;
 
   ngOnInit() {
   }
