52987df7ed5dfac3fe759cd6bbc38efbe7555ff4
mhmdtshref <mhmdtshref@gmail.com>
2019-09-03 20:36:40 +0300

    Import and use technologies data from the json file #222

--- client/src/app/pages/technologies/technologies.component.ts
+++ client/src/app/pages/technologies/technologies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { technologies } from '../../../appData.json';
 
 @Component({
   selector: 'app-technologies',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class TechnologiesComponent implements OnInit {
 
   constructor() { }
+
+  technologiesData = technologies;
 
   ngOnInit() {
   }
