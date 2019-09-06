fa6c6f24163d259825d30f120701b356a6829616
mhmdtshref <mhmdtshref@gmail.com>
2019-09-06 20:14:42 +0300

    Add socials section and about section to social page component #228

--- client/src/app/pages/socials/socials.component.ts
+++ client/src/app/pages/socials/socials.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { socials } from '../../../appData.json';
 
 @Component({
   selector: 'app-socials',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class SocialsComponent implements OnInit {
 
   constructor() { }
+
+  socialsData = socials;
 
   ngOnInit() {
   }
